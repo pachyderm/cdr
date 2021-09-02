25a01c353fda7ea6c996daf3198f2afad91f2e3b
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2021-09-02 11:12:16 -0700

    Added generated python protos and some python stubs.

---
+++ cdr.py
@@ -0,0 +1,20 @@
+from typing import Iterator
+from cdr_pb2 import Ref
+
+class Resolver:
+
+    def deref(self, ref: Ref) -> Iterator:
+        """ NOTE: I would implement this as a context manager, but this is a simple stand in. """
+        raise NotImplementedError
+
+
+def is_immutable(ref: Ref) -> bool:
+    raise NotImplementedError
+
+
+def min_size(ref: Ref) -> int:
+    raise NotImplementedError
+
+
+def max_size(ref: Ref) -> int:
+    raise NotImplementedError
